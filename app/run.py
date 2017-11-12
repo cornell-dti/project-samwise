975b81b06810d60ed4200662dd03a9ca27329803
Gautam Mekkat <Blue9@users.noreply.github.com>
2017-11-12 15:39:21 -0500

    Renamed UserData database

--- app/run.py
+++ app/run.py
@@ -20,7 +20,7 @@ login_manager.session_protection = 'strong'
 
 
 class UserData(db.Model, UserMixin):
-    __tablename__ = 'samwisedb.UserData'
+    __tablename__ = 'UserData'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     name = db.Column(db.String(255), nullable=True)

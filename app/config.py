3ba0986161e96f95e6a6377d3cf3a2fb70cfc9de
Gautam Mekkat <Blue9@users.noreply.github.com>
2018-03-25 14:09:08 -0400

    Added callback uri env var

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ COLORS = ['4286f4', 'ed4040', 'd33fed', '17b236', '17c2ed']
 
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-REDIRECT_URI = 'http://localhost:5000/gauth_callback'
+REDIRECT_URI = '%s/gauth_callback' % (os.getenv('CALLBACK_URI', 'http://localhost:5000'))
 AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
 TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
 USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
